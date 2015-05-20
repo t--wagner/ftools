0fb887c13bb26d526de6462a062f1434e36d6ff8
wagner <wagner@Welsh.nano.uni-hannover.de>
2015-05-20 16:48:25 +0200

    ftools

--- ftools.py
+++ ftools.py
@@ -5,46 +5,45 @@ import glob
 from collections import OrderedDict
 import operator
 
+try:
+   import cPickle as pickle
+except:
+   import pickle
+
 # Wrappers
 from os.path import exists, dirname, basename
 from os import makedirs as mkdir
 
 
-def file_list(file_pattern, *sorted_args, **sorted_kwargs):
-    """List of sorted filenames in path, based on pattern.
+def extension(filename):
+    """Get extension from filename.
 
-    """
-    files = glob.glob(file_pattern)
-    return sorted(files, *sorted_args, **sorted_kwargs)
-
-
-def file_tuple(filenames, index=0, split='_'):
-    """Create ordered dictonary from filenames in based on pattern.
-
+    Extension is everything from the last dot to the end, ignoring leading
+    dots. Extension may be empty.
     """
 
-    files = list()
+    return os.path.splitext(filename)[1]
 
-    if isinstance(index, (slice, int)):
-        ig = operator.itemgetter(index)
+
+def split(filename, extension=False):
+    dirname, basename = os.path.split(filename)
+
+    if extension:
+        basename, filetype = os.path.splitext(basename)
+        return dirname, basename, filetype
     else:
-        ig = operator.itemgetter(*index)
-
-    for filename in filenames:
-        basename_str = basename(filename)
-        keys = ig(basename_str.split(split))
-        key = split.join(keys)
-        files.append((key, filename))
-
-    return files
+        return dirname, basename
 
 
-def file_dict(filenames, index=0, split='_'):
-    return dict(file_tuple(filenames, index, split))
+def index(filename, digits=3, start=0, stop=None, inc=1, seperator='_'):
+    value = start
+    dirname, basename = split(filename)
 
-
-def file_odict(filenames, index=0, split='_'):
-    return OrderedDict(file_tuple(filenames, index, split))
+    while ((stop is None) or (value < stop)):
+        indexed_basename = '{0:0{1}d}{2}{3}'.format(value, digits, seperator, basename)
+        indexed_filename = '{}/{}'.format(dirname, indexed_basename)
+        yield indexed_filename
+        value += inc
 
 
 def mkfile(filename, override=False):
@@ -67,144 +66,80 @@ def mkfile(filename, override=False):
     return open(filename, 'w')
 
 
-def filetype(filename):
-    """Get type extension from filename.
+def flist(file_pattern, *sorted_args, **sorted_kwargs):
+    """List of sorted filenames in path, based on pattern.
 
-    Extension is everything from the last dot to the end, ignoring leading
-    dots. Extension may be empty.
+    """
+    files = glob.glob(file_pattern)
+    return sorted(files, *sorted_args, **sorted_kwargs)
+
+
+def ftuple(filenames, index=0, split='_'):
+    """Create ordered dictonary from filenames in based on pattern.
+
     """
 
-    return os.path.splitext(filename)[1]
+    files = list()
 
-
-def split(filename, filetype=False):
-    dirname, basename = os.path.split(filename)
-
-    if filetype:
-        basename, filetype = os.path.splitext(basename)
-        return dirname, basename, filetype
+    if isinstance(index, (slice, int)):
+        ig = operator.itemgetter(index)
     else:
-        return dirname, basename
+        ig = operator.itemgetter(*index)
 
-def index_str(positions, index):
-    """Return an index string with a number of positions.
+    for filename in filenames:
+        basename_str = basename(filename)
+        keys = ig(basename_str.split(split))
+        if isinstance(keys, str):
+            key = keys
+        else:
+            key = split.join(keys)
+        files.append((key, filename))
 
-    """
-
-    # Make index value to string
-    val_str = str(index)
-
-    # Check if index is out of position range
-    if len(val_str) > positions:
-        raise ValueError('index out of position range')
-
-    # Get the pre zeros
-    zero_str = '0' * (positions - len(val_str))
-
-    # Return index string
-    return zero_str + val_str
+    return files
 
 
-class IndexerBase(object):
-
-    def __init__(self, filename, positions=3, start=0, increment=1):
-
-        # Extract path and filename from it
-        self._path = os.path.dirname(filename)
-        self._basename = os.path.basename(filename)
-        self._positions = positions
-        self._start = start
-        self._value = start
-        self._increment = increment
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        pass
-
-    @property
-    def path(self):
-        return self._path
-
-    @property
-    def basename(self):
-        return self._basename
-
-    @property
-    def positions(self):
-        return self._positions
-
-    @property
-    def start(self):
-        return self._start
-
-    @property
-    def value(self):
-        return self._value
-
-    @property
-    def increment(self):
-        return self._increment
+def fdict(filenames, index=0, split='_'):
+    return dict(ftuple(filenames, index, split))
 
 
-class DirectoryIndexer(IndexerBase):
-
-    def next(self):
-
-        # Create index string
-        try:
-            index = index_str(self._positions, self._value)
-        except ValueError:
-            raise StopIteration
-
-        # Split path
-        split = self._path.split('/')
-
-        # Add index to last folder
-        split[-1] = index + '_' + split[-1]
-
-        # Put everything back together
-        path = ''
-        for part in split:
-            path = path + part + '/'
-
-        # Increment the index value
-        self._value += 1
-
-        # Return filename
-        return path + self._basename
+def fodict(filenames, index=0, split='_'):
+    return OrderedDict(ftuple(filenames, index, split))
 
 
-class BasenameIndexer(IndexerBase):
-
-    def next(self):
-
-        # Create index string
-        try:
-            index = index_str(self._positions, self._value)
-        except ValueError:
-            raise StopIteration
-
-        # Put filename together
-        filename = index + '_' + self._basename
-        if self._path:
-            filename = self._path + '/' + filename
-
-        # Increment the index value
-        self._value += 1
-
-        # Return filename
-        return filename
+def pdump(obj, file, override=False, protocol=0):
+    if isinstance(file, str):
+        with mkfile(file, override) as fobj:
+            pickle.dump(obj, fobj, protocol)
+    else:
+        pickle.dump(obj, file, protocol)
 
 
-def read_file(filename, strip=True):
+def pload(file):
+    if isinstance(file, str):
+        with open(file) as fobj:
+            obj = pickle.load(fobj)
+    else:
+        obj = pickle.load(file)
+
+    return obj
+
+
+def read(filename, nr=None, strip=True):
     """Read file into string.
 
     """
 
-    with open(filename, 'r') as fobj:
-        file_str = fobj.read()
+    with open(filename) as fobj:
+        if nr:
+            lines = []
+            for x in xrange(nr):
+                try:
+                    lines.append(next(fobj))
+                except StopIteration:
+                    break
+            file_str = ''.join(lines)
+        else:
+            file_str = fobj.read()
 
     if strip:
         file_str = file_str.strip()
